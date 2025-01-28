ab766ef451865a66879fbed3c3277bd1c0d9851c
SofiiaHlushchak <glushchak02@gmail.com>
2025-01-28 14:44:48 +0100

    refactor: Add custom styles to Tailwind config and integrate Tailwind CSS plugin with Prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FE724C',
+        secondary: '#FFC529',
+        whiteTransparent: '#FFFFFF33',
+        gray: {
+          dark: '#1A1D26',
+          'ultra-light': '#F6F6F6',
+          abbey: '#515154',
+          manatee: '#9796A1',
+        },
+        pink: {
+          light: '#ECDCDC',
+        },
+        orange: {
+          custom: '#FE724C',
+        },
+      },
+      fontSize: {
+        micro: '7px',
+        mini: '9px',
+        tiny: '10px',
+      },
+      backdropBlur: {
+        '5xl': '5px',
+      },
+    },
   },
   plugins: [],
 };
