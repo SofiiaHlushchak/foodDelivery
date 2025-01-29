348d42581cdc6b55497b6f2fe63db7e17ea15d0a
SofiiaHlushchak <glushchak02@gmail.com>
2025-01-29 21:44:33 +0100

    feat: Added data loading with RxJS

--- src/app/services/restaurants.service.ts
+++ src/app/services/restaurants.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { CardInterface } from '../shared/interfaces/card.interface';
 import { environment } from '../environments/environment';
 import { CardTypeEnum } from '../shared/enums/card-type.enum';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,27 +12,40 @@ import { Observable } from 'rxjs';
 export class RestaurantsService {
   private baseUrl = environment.API_URL;
 
+  private restaurantsSubject = new BehaviorSubject<CardInterface[]>([]);
+  private dishesSubject = new BehaviorSubject<CardInterface[]>([]);
+
+  public restaurants$ = this.restaurantsSubject.asObservable();
+  public dishes$ = this.dishesSubject.asObservable();
+
   constructor(private http: HttpClient) {}
 
   getRestaurants(): Observable<CardInterface[]> {
-    return this.http.get<CardInterface[]>(`${this.baseUrl}/restaurants`);
+    return this.http
+      .get<CardInterface[]>(`${this.baseUrl}/restaurants`)
+      .pipe(tap(restaurants => this.restaurantsSubject.next(restaurants)));
   }
 
   getDishes(): Observable<CardInterface[]> {
-    return this.http.get<CardInterface[]>(`${this.baseUrl}/dishes`);
+    return this.http.get<CardInterface[]>(`${this.baseUrl}/dishes`).pipe(
+      map(dishes => dishes.sort((a, b) => (a.price ?? 0) - (b.price ?? 0))),
+      tap(dishes => this.dishesSubject.next(dishes))
+    );
   }
 
-  toggleFavourite(
-    itemId: string,
-    type: CardTypeEnum,
-    restaurants: CardInterface[],
-    dishes: CardInterface[]
-  ) {
-    const listItems = type === CardTypeEnum.Restaurant ? restaurants : dishes;
-    const item = listItems.find(i => i.id === itemId);
-
-    if (item) {
-      item.isFavourite = !item.isFavourite;
-    }
+  toggleFavourite(itemId: string, type: CardTypeEnum): void {
+    const subject =
+      type === CardTypeEnum.Restaurant
+        ? this.restaurantsSubject
+        : this.dishesSubject;
+    subject.next(
+      subject
+        .getValue()
+        .map(item =>
+          item.id === itemId
+            ? { ...item, isFavourite: !item.isFavourite }
+            : item
+        )
+    );
   }
 }
