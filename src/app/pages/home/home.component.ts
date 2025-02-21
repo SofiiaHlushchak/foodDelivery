a0c80a7a30c9e19a705dc145948c7f541eb59cf9
SofiiaHlushchak <glushchak02@gmail.com>
2025-02-21 11:06:39 +0100

    feat: add filters and search for restaurants

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RestaurantsService } from '../../services/restaurants.service';
 import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { CardTypeEnum } from '../../shared/enums/card-type.enum';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { PrimaryCardComponent } from '../../shared/components/primary-card/primary-card.component';
@@ -8,40 +9,120 @@ import { RouterModule } from '@angular/router';
 import { RestaurantInterface } from '../../shared/interfaces/restaurant.interface';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { LoadingService } from '../../services/loading.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FilterSidebarComponent } from '../../shared/components/filter-sidebar/filter-sidebar.component';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [
     AsyncPipe,
     PrimaryCardComponent,
     CommonModule,
     RouterModule,
     MatProgressBarModule,
+    ReactiveFormsModule,
+    FilterSidebarComponent,
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  configAvatar = true;
+  private restaurantsService = inject(RestaurantsService);
+  private fb = inject(FormBuilder);
+
+  searchForm!: FormGroup;
+  restaurants$: Observable<RestaurantInterface[]> =
+    this.restaurantsService.restaurants$;
+  isLoading$: Observable<boolean> = inject(LoadingService).isLoading$;
   isSidebarOpen = false;
-  restaurants$!: Observable<RestaurantInterface[]>;
+  isFiltersSidebarOpen = false;
+
+  categories = [
+    'burger',
+    'chicken',
+    'fast food',
+    'coffee',
+    'tea',
+    'drinks',
+    'ice-cream',
+    'donut',
+    'pizza',
+    'italian',
+    'pasta',
+    'taco',
+    'burito',
+    'mexican',
+    'hot dog',
+  ];
 
   CardTypeEnum = CardTypeEnum;
 
-  isLoading: Observable<boolean> = inject(LoadingService).isLoading$;
+  ngOnInit(): void {
+    this.searchForm = this.fb.group({
+      searchQuery: [''],
+      categories: this.fb.array(
+        this.categories.map(() => this.fb.control(false))
+      ),
+      sortBy: [''],
+      rating: [''],
+      priceFrom: [null],
+      priceTo: [null],
+    });
 
-  private restaurantsService = inject(RestaurantsService);
+    this.restaurantsService.getCachedRestaurants().subscribe();
+    this.searchForm
+      .get('searchQuery')!
+      .valueChanges.pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe(() => this.applyFilters());
 
-  ngOnInit() {
-    this.restaurants$ = this.restaurantsService.restaurants$;
-    this.restaurantsService.getRestaurants().subscribe();
+    this.searchForm.get('categories')!.valueChanges.subscribe(categories => {
+      this.applyFilters(categories);
+    });
+  }
+
+  getCategoryValue(index: number): boolean {
+    return this.searchForm.get('categories')?.value[index] || false;
+  }
+
+  applyFilters(updatedCategories?: boolean[]): void {
+    const { searchQuery, sortBy, categories, rating, priceFrom, priceTo } =
+      this.searchForm.value;
+    const selectedCategories = updatedCategories || categories;
+
+    const filteredCategories = this.categories.filter(
+      (_, i) => selectedCategories[i]
+    );
+
+    this.restaurantsService.setFilters(
+      searchQuery?.trim(),
+      sortBy,
+      filteredCategories,
+      rating,
+      priceFrom,
+      priceTo
+    );
+  }
+
+  onApplySort(): void {
+    this.applyFilters();
+    this.toggleFiltersSidebar();
+  }
+
+  onResetFilters() {
+    this.searchForm.reset();
+    this.toggleFiltersSidebar();
+  }
+
+  toggleSidebar(): void {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  toggleFiltersSidebar(): void {
+    this.isFiltersSidebarOpen = !this.isFiltersSidebarOpen;
   }
 
   onToggleFavourite(itemId: string): void {
     this.restaurantsService.toggleFavourite(itemId);
-  }
-
-  toggleSidebar() {
-    this.isSidebarOpen = !this.isSidebarOpen;
   }
 }
--- src/app/services/restaurants.service.ts
+++ src/app/services/restaurants.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, BehaviorSubject, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { environment } from '../environments/environment';
 import { RestaurantInterface } from '../shared/interfaces/restaurant.interface';
 
@@ -16,10 +16,60 @@ export class RestaurantsService {
 
   constructor(private http: HttpClient) {}
 
-  getRestaurants(): Observable<RestaurantInterface[]> {
+  getCachedRestaurants(): Observable<RestaurantInterface[]> {
+    const cachedRestaurants = this.restaurantsSubject.getValue();
+
+    return cachedRestaurants?.length
+      ? of(cachedRestaurants)
+      : this.getRestaurants();
+  }
+
+  getRestaurants(
+    query = '',
+    sortBy = '',
+    categories: string[] = [],
+    rating = '',
+    priceFrom: number | null = null,
+    priceTo: number | null = null
+  ): Observable<RestaurantInterface[]> {
+    let params = new HttpParams();
+    if (query) params = params.set('name', query);
+    if (sortBy) params = params.set('sortBy', sortBy);
+    if (categories.length)
+      params = params.set('categories', categories.join(','));
+    if (rating) params = params.set('rating', rating);
+    if (priceFrom) params = params.set('priceFrom', priceFrom.toString());
+    if (priceTo) params = params.set('priceTo', priceTo.toString());
+
     return this.http
-      .get<RestaurantInterface[]>(this.restaurantsUrl)
-      .pipe(tap(restaurants => this.restaurantsSubject.next(restaurants)));
+      .get<RestaurantInterface[]>(this.restaurantsUrl, { params })
+      .pipe(
+        tap(restaurants => {
+          this.restaurantsSubject.next(restaurants);
+        }),
+        catchError(error => {
+          console.error('Error fetching restaurants:', error);
+          return [];
+        })
+      );
+  }
+
+  setFilters(
+    query = '',
+    sortBy = '',
+    categories: string[] = [],
+    rating = '',
+    priceFrom: number | null = null,
+    priceTo: number | null = null
+  ): void {
+    this.getRestaurants(
+      query,
+      sortBy,
+      categories,
+      rating,
+      priceFrom,
+      priceTo
+    ).subscribe();
   }
 
   getRestaurantById(id: string): Observable<RestaurantInterface> {
